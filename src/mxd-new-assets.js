90754320eb19068165dc56b17d4294788af495cf
Christoph Herrmann <developer@dragonprojects.de>
2016-11-24 08:06:25 +0100

    Refactoring to avoid endless increasing asset id array and optimize to 2 requests

--- src/mxd-new-assets.js
+++ src/mxd-new-assets.js
@@ -12,84 +12,72 @@ module.exports = (RED) => {
 
     const heimdall = new Heimdall({ apikey: config.apikey, appid: config.appid });
 
-    const oldAssets = new Set();
+    let lastRunAssets = new Set();
     let timeoutHandle;
 
     async function RUN() {
       node.log('start check for new assets');
 
-      const requests = [];
+      const queries = [];
 
-      if (config['area-package'] && config['type-movies']) {
-        node.log('include check for package movies');
+      if (['all', 'movies'].includes(config.content)) {
+        node.log('include check for movies');
         const query = (new AssetsQuery())
-          .filter('new')
-          .filter('notUnlisted')
-          .filter('hasPackageContent')
-          .filter('movies')
-          .sort('activeLicenseStart', 'desc');
-        requests.push(heimdall.getAssets(query));
+          .filter('movies');
+        queries.push(query);
       }
 
-      if (config['area-package'] && config['type-seasons']) {
-        node.log('include check for package seasons');
+      if (['all', 'seasons'].includes(config.content)) {
+        node.log('include check for seasons');
         const query = (new AssetsQuery())
-          .filter('new')
-          .filter('notUnlisted')
-          .filter('hasPackageContent')
-          .filter('seasons')
-          .sort('activeLicenseStart', 'desc');
-        requests.push(heimdall.getAssets(query));
+          .filter('seasons');
+        queries.push(query);
       }
 
-      if (config['area-store'] && config['type-movies']) {
-        node.log('include check for store movies');
-        const query = (new AssetsQuery())
-          .filter('new')
-          .filter('notUnlisted')
-          .filter('availableWithoutPackage')
-          .filter('movies')
-          .sort('activeLicenseStart', 'desc');
-        requests.push(heimdall.getAssets(query));
+      if (config.area !== 'all') {
+        node.log(`restrict area to ${config.area}`);
+        queries.forEach((query) => {
+          query
+            .filter({ package: 'hasPackageContent', store: 'availableWithoutPackage' }[config.area]);
+        });
       }
 
-      if (config['area-store'] && config['type-seasons']) {
-        node.log('include check for store seasons');
-        const query = (new AssetsQuery())
+      const requests = queries.map((query) => {
+        query
           .filter('new')
           .filter('notUnlisted')
-          .filter('availableWithoutPackage')
-          .filter('seasons')
           .sort('activeLicenseStart', 'desc');
-        requests.push(heimdall.getAssets(query));
-      }
-
-      if (requests.length === 0) {
-        node.warn('need at least one area and type combination enabled');
+        return heimdall.getAssets(query);
+      });
+      let responses;
+      try {
+        responses = await Promise.all(requests);
+      } catch (e) {
+        node.warn(`requesting error, skip this run (${e.message})`);
         return;
       }
-
-      const responses = await Promise.all(requests);
 
       const assets = responses.reduce((a, b) => a.concat(b));
       if (assets.length === 0) {
         node.warn('no assets in the responses');
         return;
       }
-      if (oldAssets.size) {
-        assets.forEach((asset) => {
-          if (!oldAssets.has(asset.id)) {
-            node.log(`send new asset (id: ${asset.id})`);
-            node.send({ payload: asset });
-            oldAssets.add(asset.id);
-          }
-        });
-      } else {
+
+      let firstRun = false;
+      if (!lastRunAssets.size) {
         node.log('first run');
-        assets.forEach((asset) => {
-          oldAssets.add(asset.id);
-        });
+        firstRun = true;
       }
+      const currentRunAssets = new Set();
+      assets.forEach((asset) => {
+        if (!firstRun && !lastRunAssets.has(asset.id)) {
+          node.log(`send new asset (id: ${asset.id})`);
+          node.send({ payload: asset });
+        }
+        currentRunAssets.add(asset.id);
+      });
+      lastRunAssets = currentRunAssets;
+
       node.log('end check for new assets');
       timeoutHandle = setTimeout(RUN, config.interval * 1000);
     }
