f7ca09bf383112b38579e46d742deae180d55b04
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-12-14 15:54:55 +0100

    node-red-contrib-dnode

--- src/mxd-new-assets.js
+++ src/mxd-new-assets.js
@@ -1,105 +1,61 @@
-module.exports = (RED) => {
-  RED.nodes.registerType('mxd-new-assets', function NODE(config) {
-    RED.nodes.createNode(this, config);
-    const node = this;
-    node.status({});
+const DNode = require('node-red-contrib-dnode');
 
-    if (!config.interval) {
-      node.status({ fill: 'red', shape: 'dot', text: 'config is missing' });
-      node.error('config is missing');
-      return;
+module.exports = DNode.createNode('mxd-new-assets', (dnode) => {
+  const { interval, area, content } = dnode.getConfigs(['interval', 'area', 'content']);
+  const { AssetsQuery, heimdall } = dnode.getServices('heimdall');
+
+  let lastRunAssets = new Set();
+  dnode.onTick(interval, async () => {
+    const queries = [];
+
+    if (['all', 'movies'].includes(content)) {
+      const query = (new AssetsQuery())
+        .filter('movies');
+      queries.push(query);
     }
-    node.log(`initialize mxd-new-assets node with an interval of ${config.interval} seconds`);
-
-    const { AssetsQuery, heimdall } = RED.nodes.getNode(config.heimdall);
-    if (!AssetsQuery || !heimdall) {
-      node.status({ fill: 'red', shape: 'dot', text: 'heimdall is missing' });
-      node.error('heimdall is missing');
-      return;
+    if (['all', 'seasons'].includes(content)) {
+      const query = (new AssetsQuery())
+        .filter('seasons');
+      queries.push(query);
     }
 
-    let lastRunAssets = new Set();
-    let timeoutHandle;
-
-    async function RUN() {
-      node.status({ fill: 'grey', shape: 'dot', text: 'check for new assets...' });
-      node.log('start check for new assets');
-
-      const queries = [];
-
-      if (['all', 'movies'].includes(config.content)) {
-        node.log('include check for movies');
-        const query = (new AssetsQuery())
-          .filter('movies');
-        queries.push(query);
-      }
-
-      if (['all', 'seasons'].includes(config.content)) {
-        node.log('include check for seasons');
-        const query = (new AssetsQuery())
-          .filter('seasons');
-        queries.push(query);
-      }
-
-      if (config.area !== 'all') {
-        node.log(`restrict area to ${config.area}`);
-        queries.forEach((query) => {
-          query
-            .filter({ package: 'hasPackageContent', store: 'availableWithoutPackage' }[config.area]);
-        });
-      }
-
-      const requests = queries.map((query) => {
+    if (area !== 'all') {
+      queries.forEach((query) => {
         query
-          .filter('new')
-          .filter('notUnlisted')
-          .sort('activeLicenseStart', 'desc');
-        return heimdall.getAssets(query);
+          .filter({ package: 'hasPackageContent', store: 'availableWithoutPackage' }[area]);
       });
-      try {
-        const responses = await Promise.all(requests);
-
-        const assets = responses.reduce((a, b) => a.concat(b));
-        if (assets.length === 0) {
-          throw new Error('no assets in the responses');
-        }
-
-        let firstRun = false;
-        if (!lastRunAssets.size) {
-          node.log('first run');
-          firstRun = true;
-        }
-        const currentRunAssets = new Set();
-        const newAssets = new Set();
-        assets.forEach((asset) => {
-          if (!firstRun && !lastRunAssets.has(asset.id)) {
-            node.log(`new asset (id: ${asset.id})`);
-            newAssets.add(asset);
-          }
-          currentRunAssets.add(asset.id);
-        });
-        lastRunAssets = currentRunAssets;
-
-        node.status({ fill: 'green', shape: 'dot', text: `sent ${newAssets.size} new assets` });
-        if (newAssets.size > 0) {
-          node.log(`send ${newAssets.size} new assets`);
-          node.send({ payload: Array.from(newAssets) });
-        }
-      } catch (e) {
-        node.status({ fill: 'yellow', shape: 'dot', text: 'requesting error' });
-        node.warn(`requesting error, skip this run (${e.message})`);
-      }
-
-      node.log('end check for new assets');
-      timeoutHandle = setTimeout(RUN, config.interval * 1000);
     }
 
-    process.nextTick(RUN);
-
-    node.on('close', () => {
-      if (timeoutHandle) {
-        clearTimeout(timeoutHandle);
-      }
+    const requests = queries.map((query) => {
+      query
+        .filter('new')
+        .filter('notUnlisted')
+        .sort('activeLicenseStart', 'desc');
+      return heimdall.getAssets(query);
     });
+    const responses = await Promise.all(requests);
+    const assets = responses.reduce((a, b) => a.concat(b));
+    if (assets.length === 0) {
+      throw new Error('no assets in the responses');
+    }
+
+    let firstRun = false;
+    if (!lastRunAssets.size) {
+      firstRun = true;
+    }
+    const currentRunAssets = new Set();
+    const newAssets = new Set();
+    assets.forEach((asset) => {
+      if (!firstRun && !lastRunAssets.has(asset.id)) {
+        newAssets.add(asset);
+      }
+      currentRunAssets.add(asset.id);
+    });
+    lastRunAssets = currentRunAssets;
+
+    dnode.setStatus('green', `sent ${newAssets.size} new assets`);
+    if (newAssets.size > 0) {
+      dnode.sendMessage({ payload: Array.from(newAssets) });
+    }
   });
-};
+});
